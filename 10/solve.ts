f5155919a4e4014ed22cb2c87c2949f1e860a4f0
Gabriel Grinberg <gabrielg@wix.com>
2020-12-10 08:16:59 +0200

    10b

--- 10/solve.ts
+++ 10/solve.ts
@@ -10,15 +10,6 @@ const parseInput = (raw: string) => {
 		.filter((v) => !!v)
 		.map(Number);
 
-	// const grid = new Map();
-	// rows.forEach((row, y) => {
-	// 	row.split('').forEach((cell, x)  => {
-	// 		const value = cell;
-	// 		setCell({x, y}, value, grid);
-	// 	})
-	// }
-
-	// console.log(rows);
 	return rows;
 }
 
@@ -29,42 +20,52 @@ export const solve = (raw: string): any => {
 
 	input.push(adapter);
 
-	let curr = 0;
+	const diffs = input.map((n, i) => {
+		const last = input[i - 1] || 0;
+		return n - last;
+	});
 
-	let d1 = 0;
-	let d3 = 0;
+	console.log(diffs.join(' '));
 
-	for (let i = 0; i< input.length; i++) {
-		const v = input[i];
-		console.log(curr, v);
-		
-		const diff = v - curr;
-		// curr+=v;
-		switch (diff) {
-			case 1:
-				d1++;
-				curr += 1;
-				break;
-			case 2:
-				curr += 2;
-				// d1++;
-				break;
-			case 3:
-				curr += 3;
-				d3++;
-				break;
-			default:
-				console.log('diff', diff);
-				throw 'oops'
+    // 4 ^ 1 ^ 4 ^ 2 ^ 3 ^ 4 ^ 4
+
+	let ic = 0;
+	const bob = []
+	for (let i = 0; i < diffs.length; i++) {
+		const v = diffs[i];
+		if (v === 1) {
+			ic++
+		} else if (v === 3) {
+			ic && bob.push(ic);
+			ic = 0;
+		} else {
+			throw 'bob'
 		}
-
-
-		
 	}
+	console.log(bob);
 
-	// while ()
+	return bob .reduce((acc, b) => {
 
-	return d1 * d3;
+		// 1 -> 1
+		// 2 -> 2
+		// 3 -> 1 + 1 + 1 | 1 + 2 | 2 + 1 | 3
+		// 4 -> 1 + 1 + 1 + 1 | 1 + 3 | 1 + 
+
+		const m = {
+			1: 1,
+			2: 2,
+			3: 4,
+			4: 7
+		};
+
+		return acc * m[b];
+
+	}, 1)
+
+
+
+
+	
 };
 
 describe('bob', () => {	
@@ -80,7 +81,7 @@ describe('bob', () => {
 6
 12
 4`;
-
+// 
 		assert.equal(solve(input), 8);
 	});
 
@@ -130,7 +131,7 @@ describe('bob', () => {
 
 		// const expected = 42;
 
-		// assert.equal(solve(puzzleInput), 'bob');
+		assert.equal(solve(puzzleInput), 'bob');
 	});
 });
 
