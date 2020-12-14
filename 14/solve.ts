3075f4d8539ec32070d434084bde50ab4861faa2
Gabriel Grinberg <gabrielg@wix.com>
2020-12-14 07:39:45 +0200

    bob

--- 14/solve.ts
+++ 14/solve.ts
@@ -1,5 +1,5 @@
 import { puzzleInput } from "./puzzle-input";
-import {assert} from 'chai';
+import { assert } from 'chai';
 import { setCell } from "./utils";
 
 
@@ -20,18 +20,18 @@ const parseInput = (raw: string) => {
 		}
 
 		const [location, value] = r.match(/mem\[(\d+)\] = (\d+)/).slice(1).map(Number);
-		return {location, value};
+		return { location, value };
 	});
 
-		const  [maskRaw, ...rest] = rows;
+	const [maskRaw, ...rest] = rows;
 
-		const cmds = rest.map(r => {
-			const [location, value] = r.match(/mem\[(\d+)\] = (\d+)/).slice(1).map(Number);
-			return {location, value};
-		})
+	const cmds = rest.map(r => {
+		const [location, value] = r.match(/mem\[(\d+)\] = (\d+)/).slice(1).map(Number);
+		return { location, value };
+	})
 
-		return {mask: maskRaw.replace('mask = ', ''), cmds};
-		// .map(Number);
+	return { mask: maskRaw.replace('mask = ', ''), cmds };
+	// .map(Number);
 
 	// const grid = new Map();
 	// rows.forEach((row, y) => {
@@ -48,43 +48,78 @@ const parseInput = (raw: string) => {
 export const solve = (raw: string): any => {
 	const cmds = parseInput(raw) as any;
 
-	let mem = [];
+	let mem = {};
 	let mask = '';
 
 	console.log(mask);
+
+	const cache = new Map();
+
+	const calcPermutations = (add: string) => {
+		if (!cache.get(add)) {
+			const permutations = [];
+
+			const queue = [add];
+
+			while (queue.length) {
+				const next = queue.shift();
+
+				if (next && !next.includes('X')) {
+					permutations.push(next);
+					continue;
+				}
+
+				const nextIdx = next.indexOf('X');
+
+				const n1 = next.split('');
+				const n2 = next.split('');
+				n1[nextIdx] = '0';
+				n2[nextIdx] = '1';
+				queue.push(n1.join(''));
+				queue.push(n2.join(''));
+			}
+			cache.set(add, permutations)
+		}
+		return cache.get(add);
+	}
 
 	cmds.forEach((cmd) => {
 		if (cmd.mask) {
 			mask = cmd.mask;
 			return;
 		}
-		const {location, value } = cmd;
+		const { location, value } = cmd;
 
 
-		const bin = value.toString(2).split('').reverse();
-		const newValue = mask.split('').reverse().map(((c, i) => {
-			const v = bin[i] || '0';
-			if (c === 'X') return v;
+		const address = location.toString(2).split('').reverse();
+
+		const newAddress = mask.split('').reverse().map(((c, i) => {
+			const v = address[i] || '0';
+			if (c === '0') return v;
 			return c;
 		})).reverse().join('');
 
-		console.log(newValue);
-		
 
 
-		mem[location] = parseInt(newValue, 2);
+		const permutations = calcPermutations(newAddress);// console.log(permutations);
+
+		permutations.forEach((add) => {
+
+
+			// const v = BigInt(`0b${add}`);
+			mem[`0b${add}`] = value;
+		})
 	})
-	console.log(mem);
 
-	return mem.reduce((acc, p) => acc + p, 0);
+	return Object.values(mem).reduce((acc: any, p: any) => acc + BigInt(p), BigInt(0));
 };
 
-describe('bob', () => {	
+describe('bob', () => {
 	it('works for test case 1', () => {
-		const input = `mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
-mem[8] = 11
-mem[7] = 101
-mem[8] = 0`;
+		const input = `mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1`;
 
 		assert.equal(solve(input), 208);
 	});
@@ -92,14 +127,14 @@ mem[8] = 0`;
 	// it('works for test case 2', () => {
 	// 	const input = `HERE`;
 
-	// 	assert.equal(solve(input), 424242);
+	// 	assert.equal(solve(input), 424242);r
 	// });
 
 	it('show the answer when ready!', () => {
 
 		// const expected = 42;
 
-		assert.equal(solve(puzzleInput), 'bob');
+		// assert.equal(solve(puzzleInput), 'bob');
 	});
 });
 
