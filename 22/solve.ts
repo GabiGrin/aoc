546d7ee215ed2ad3a0f81801eec822cc54b3f13e
Gabriel Grinberg <gabrielg@wix.com>
2020-12-22 07:42:05 +0200

    cleanups

--- 22/solve.ts
+++ 22/solve.ts
@@ -1,7 +1,5 @@
 import { puzzleInput } from "./puzzle-input";
 import {assert} from 'chai';
-import { setCell } from "./utils";
-
 
 const parseInput = (raw: string) => {
 	const rows = raw
@@ -18,37 +16,26 @@ const parseInput = (raw: string) => {
 export const solve = (raw: string): any => {
 	const original = parseInput(raw);
 
-
-	const playGame = (p1, p2, depth = 0) => {
+	const playGame = (p1, p2) => {
 		const visitedCards = new Set();
 
 		const addVisited = (p1, p2) => visitedCards.add(p1.join(',') + '|' + p2.join(','));
 		const hasVisited = (p1, p2) => visitedCards.has(p1.join(',') + '|' + p2.join(','));
 
-		let round = 0;
-
 		while (p1.length > 0 && p2.length > 0) {
-
-			// console.log(`Playing game ${depth} round ${round++}`);
 			if (hasVisited(p1, p2)) {
-				console.log(p1, p2);
-				return ['p1', p1, p2, p1];
+				return ['p1', p1];
 			} 
 			addVisited(p1, p2);
 
 			const n1 = p1.shift();
 			const n2 = p2.shift();
-	
-			const shouldRecurse = p1.length >= n1 && p2.length >= n2;
 
-
-			if (shouldRecurse) {
+			if (p1.length >= n1 && p2.length >= n2) {
 				const d1 = p1.slice(0, n1);
 				const d2 = p2.slice(0, n2);
 
-				// console.log('RECURSING', depth, round, n1, n2)
-
-				const winner = playGame(d1, d2, depth + 1);
+				const winner = playGame(d1, d2);
 
 				if (winner[0] === 'p1') {
 					p1.push(n1, n2);
@@ -58,35 +45,26 @@ export const solve = (raw: string): any => {
 					throw 'wa'
 				}
 			} else {
-				// console.log(`Game ${depth} round ${round}`);
 				if (n1 > n2) {
-
-					// console.log('P1 wins\n', p1.join(', '), '\n', p2.join(', '));
 					p1.push(n1, n2);
 				} else if (n2 > n1) {
-					// console.log('P2 wins\n', p1.join(', '), '\n',p2.join(', '));
 					p2.push(n2, n1);
 				} else {
 					throw 'wa'
 				}
 			}
-	
 		}
 
 		if (p1.length > 0) {
-			return ['p1', p1, p2, p1]
+			return ['p1', p1]
 		} else {
-			return ['p2', p1, p2, p2];
+			return ['p2', p2];
 		}
 	}
 
-	const [w, a, b, deck] = playGame(original.p1, original.p2);
-
-	// console.log({w, a, b})
+	const [w, deck] = playGame(original.p1, original.p2);
 
 	return deck.reverse().map((n, i) => n * (i + 1)).reduce((a, p) => a+ p, 0);
-
-	// return input.length;
 };
 
 describe('bob', () => {	
